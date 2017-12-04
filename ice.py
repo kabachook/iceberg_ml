3c8141b7d531e3a72551a69d881772bf4c45e682
Nick Gant <beltyukovdanil@gmail.com>
2017-12-04 22:16:08 +0300

    Refactoring and little improvements

--- ice.py
+++ ice.py
@@ -1,60 +1,78 @@
+import os
+os.environ['PATH'] = f'C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v8.0\\bin;{os.environ["PATH"]}'
+
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, ELU
+from keras.layers import Dense, Dropout, Activation, Flatten, LeakyReLU
 from keras.layers import Conv2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 import numpy as np
 import pandas as pd
-import os
 
-from model import MyNet, callbacks, file_path
+from model import callbacks, file_path, MyNetv2, MyNetv3
+
 
 # Constants
 batch_size = 64
-epochs = 10
-predict = True
+epochs = 100
+predict = False
 
-train = pd.read_json("input\\train.json")
+seed = 4444
+
+data = pd.read_json("input\\train.json")
+data.inc_angle = data.inc_angle.replace('na', 0)
+data.inc_angle = data.inc_angle.astype(float).fillna(0.0)
+
+
+def gen_flow_for_two_inputs(X1, X2, y, gen):
+    genX1 = gen.flow(X1, y,  batch_size=batch_size, seed=seed)
+    genX2 = gen.flow(X1, X2, batch_size=batch_size, seed=seed)
+    while True:
+        X1i = genX1.next()
+        X2i = genX2.next()
+        # Assert arrays are equal - this was for peace of mind, but slows down training
+        # np.testing.assert_array_equal(X1i[0],X2i[0])
+        yield [X1i[0], X2i[1]], X1i[1]
 
 
 # Add band 3 = (band_1 + band_2)/2
-X_band_1 = np.array([np.array(band).astype(
-    np.float32).reshape(75, 75) for band in train["band_1"]])
-X_band_2 = np.array([np.array(band).astype(
-    np.float32).reshape(75, 75) for band in train["band_2"]])
-X_train = np.concatenate([X_band_1[:, :, :, np.newaxis], X_band_2[:, :, :, np.newaxis], ((
-    X_band_1 + X_band_2) / 2)[:, :, :, np.newaxis]], axis=-1)
-
-model = MyNet()
-
-target_train = train['is_iceberg']
-X_train_cv, X_valid, y_train_cv, y_valid = train_test_split(
-    X_train, target_train, random_state=1, train_size=0.75)
-
-#   model.fit(X_train_cv, y_train_cv, batch_size=batch_size, epochs=epochs,
-#            verbose=1, validation_data=(X_valid, y_valid), callbacks=callbacks)
+x_band_1 = np.array([np.array(band).astype(
+    np.float32).reshape(75, 75) for band in data["band_1"]])
+x_band_2 = np.array([np.array(band).astype(
+    np.float32).reshape(75, 75) for band in data["band_2"]])
+x_bands = np.concatenate([x_band_1[:, :, :, np.newaxis], x_band_2[:, :, :, np.newaxis], ((
+    x_band_1 + x_band_2) / 2)[:, :, :, np.newaxis]], axis=-1)
+data_angle = np.array(data.inc_angle)
+data_angle = data_angle / np.max(data_angle)
 
 
-if predict:
-    model.load_weights(filepath=file_path)
-    
-    score = model.evaluate(X_valid, y_valid, verbose=1)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
-    exit()
 
-    test = pd.read_json("input\\test.json")    
-    
-    X_band_test_1 = np.array([np.array(band).astype(
-        np.float32).reshape(75, 75) for band in test["band_1"]])
-    X_band_test_2 = np.array([np.array(band).astype(
-        np.float32).reshape(75, 75) for band in test["band_2"]])
-    X_test = np.concatenate([X_band_test_1[:, :, :, np.newaxis], X_band_test_2[:, :, :, np.newaxis], ((
-        X_band_test_1 + X_band_test_2) / 2)[:, :, :, np.newaxis]], axis=-1)
-    predicted_test = model.predict_proba(X_test)
+# Split data
+data_y = data['is_iceberg']
+x_train, x_test, x_train_angle, x_test_angle,  y_train, y_test = train_test_split(
+    x_bands, data_angle, data_y, random_state=seed, train_size=0.75)
 
-    submission = pd.DataFrame()
-    submission['id']=test['id']
-    submission['is_iceberg']=predicted_test.reshape((predicted_test.shape[0]))
-    submission.to_csv('sub.csv', index=False)
+# Image generator
+augmentation = ImageDataGenerator(horizontal_flip=True,
+                                  vertical_flip=True,
+                                  width_shift_range=0.1,
+                                  height_shift_range=0.1,
+                                  zoom_range=0.1,
+                                  rotation_range=360)
+
+gen_flow = gen_flow_for_two_inputs(
+    x_train, x_train_angle, y_train, augmentation)
+
+
+# model = MyNetv3(75,75)
+model = MyNetv2()
+model.fit_generator(gen_flow, epochs=epochs,
+                    verbose=1, validation_data=([x_test, x_test_angle], y_test), callbacks=callbacks,
+                    steps_per_epoch=len(x_train) / batch_size)
+
+
+model.load_weights(filepath=file_path)  # Load best result
+score = model.evaluate([x_test, x_test_angle], y_test, verbose=1)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
--- model.py
+++ model.py
@@ -1,36 +1,39 @@
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten, Activation, concatenate, AveragePooling2D, GlobalAveragePooling2D
 from keras.layers import GlobalMaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.merge import Concatenate
 from keras.models import Model
-from keras import initializers
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from keras import initializers, applications
+from keras.optimizers import Adam, Adagrad
+from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping, TensorBoard
+import keras.backend as K
 
 
 def MyNet():
     model = Sequential()
 
+    model.add(BatchNormalization(input_shape=(75, 75, 3)))
+
     # Input 75x75x3
     # Layer 1
-    model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(75, 75, 3)))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(256, (5, 5), activation='relu', input_shape=(75, 75, 3)))
+    model.add(Conv2D(128, (5, 5), activation='relu'))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.2))
 
     # Layer 2
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Dropout(0.2))
- 
-    # Layer 3
-    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.2))
 
     # Layer 3
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(Conv2D(64, (1, 1), activation='relu'))
     model.add(Dropout(0.2))
 
     model.add(Flatten())
@@ -43,21 +46,108 @@ def MyNet():
     model.add(Activation('relu'))
     model.add(Dropout(0.2))
 
+    model.add(Dense(128))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+
     model.add(Dense(1))
     model.add(Activation('sigmoid'))
 
-    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=1e-8)
     model.compile(loss='binary_crossentropy',
                   optimizer=opt, metrics=['accuracy'])
     model.summary()
     return model
 
 
-def get_callbacks(filepath, patience=2):
-    es = EarlyStopping('val_loss', patience=patience, mode="min")
-    msave = ModelCheckpoint(filepath, save_best_only=True)
-    return [es, msave]
+def MyNetv2(activation='relu'):
+    pic_input = Input(shape=(75, 75, 3))
+    ang_input = Input(shape=(1,))
+
+    cnn = BatchNormalization()(pic_input)
+    cnn = Conv2D(32, (3, 3), activation=activation)(cnn)
+    cnn = AveragePooling2D()(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Conv2D(64, (3, 3), activation=activation)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Conv2D(128, (5, 5), activation=activation)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Conv2D(256, (5, 5), activation=activation)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Conv2D(512, (1, 1), activation=activation)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = MaxPooling2D()(cnn)
+    cnn = Conv2D(512, (9, 9), activation=activation)(cnn)
+
+    # cnn = GlobalAveragePooling2D()(cnn)
+    cnn = Flatten()(cnn)
+    cnn = concatenate([cnn, ang_input])
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(2048, activation=activation)(cnn)
+    cnn = Dropout(0.5)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(1024, activation=activation)(cnn)
+    cnn = Dropout(0.5)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(512, activation=activation)(cnn)
+    cnn = Dropout(0.5)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(256, activation=activation)(cnn)
+    cnn = Dropout(0.5)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(128, activation=activation)(cnn)
+    cnn = Dropout(0.5)(cnn)
+    cnn = BatchNormalization()(cnn)
+    cnn = Dense(1, activation='sigmoid')(cnn)
+
+    simple_cnn = Model(inputs=[pic_input, ang_input], outputs=cnn)
+    # opt = Adagrad(lr=0.001,epsilon=1e-6,decay=0.)
+    opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, decay=0.)
+    simple_cnn.compile(
+        optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    simple_cnn.summary()
+    return simple_cnn
+
+
+def MyNetv3(img_width, img_height):
+    vgg = applications.VGG19(weights='imagenet', include_top=False,
+                             input_shape=(img_width, img_height, 3), pooling=None)
+
+    ang_input = Input(shape=(1,))
+    model = vgg.output
+    model = Flatten()(model)
+    model = concatenate([model, ang_input])
+    model = BatchNormalization()(model)
+    model = Dense(1024, activation='relu')(model)
+    model = Dropout(0.5)(model)
+    model = BatchNormalization()(model)
+    model = Dense(512, activation='relu')(model)
+    model = Dropout(0.5)(model)
+    model = BatchNormalization()(model)
+    model = Dense(256, activation='relu')(model)
+    model = Dropout(0.5)(model)
+    prediction = Dense(1, activation='sigmoid')(model)
+
+    model_final = Model(inputs=[vgg.input, ang_input], outputs=prediction)
+    model_final.summary()
+    model_final.compile(
+        optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model_final
+
+
+def get_callbacks(filepath, patience=2, early_stopping=False):
+    callbacks = []
+    if early_stopping:
+        callbacks.append(EarlyStopping(
+            'val_loss', patience=patience, mode="auto"))
+    callbacks.append(ModelCheckpoint(filepath, save_best_only=True))
+    callbacks.append(TensorBoard(log_dir='./logs', batch_size=32))
+    return callbacks
 
 
 file_path = ".model_weights.hdf5"
-callbacks = get_callbacks(filepath=file_path, patience=5)
+callbacks = get_callbacks(filepath=file_path, patience=20, early_stopping=True)
+
+if __name__ == "__main__":
+    MyNetv2()
+    # MyNetv3(75, 75)
